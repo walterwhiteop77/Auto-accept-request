ffb2ae5703b2d6d1b9bc858c8874851b280f5c8e
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-07-25 12:07:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channe
 
 API_ID = int(env.get('API_ID', '23789353'))
 API_HASH = env.get('API_HASH', 'fcc7f1c8b86d3e2062218a24b617e23d')
-BOT_TOKEN = env.get('BOT_TOKEN', '6111248503:AAGaXvVz8MlSB8uwc63m_pIRuxxLNV5ctis')
+BOT_TOKEN = env.get('BOT_TOKEN', '6573399998:AAHvZbhwRP9aNBPYrBWYEzumkI5mIgiXq6A')
 DB_URL = env.get('DB_URL', "mongodb+srv://jksha2024:jksha2024@cluster0.n6bdrv8.mongodb.net/?retryWrites=true&w=majority")
-ADMINS = int(env.get('ADMINS', '1746132193'))
+ADMINS = int(env.get('ADMINS', '1746132193 1353275714'))
 
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
